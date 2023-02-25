9beaf1d44fd811c7ed48ada188349c83d7e58fa2
Vikram Narra <vchandra.narra@gmail.com>
2023-02-25 17:28:29 -0500

    integrated whatsapp

---
+++ SRC/Screens/Signup/Signup_AccountCreated.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Signup_AccountCreated = () => {
+  return (
+    <View>
+      <Text>Signup_AccountCreated</Text>
+    </View>
+  )
+}
+
+export default Signup_AccountCreated
